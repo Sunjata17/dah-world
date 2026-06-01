ccf29cb73e11740871bf72b73296395efd6514e8
John Aden <drjohnsprojects@gmail.com>
2026-06-01 12:02:55 -0400

    fix: set static output mode for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel(),
 });
