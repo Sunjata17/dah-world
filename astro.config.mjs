420a130d2414c6234f25d4b7d18421a70478f9b9
John Aden <drjohnsprojects@gmail.com>
2026-06-01 12:05:56 -0400

    fix: correct vercel adapter import for v10

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
